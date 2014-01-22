3a3da856d185f25d89ce7346584a6827e7057038
GromHoll <gromholl@gmail.com>
2014-01-22 19:05:09 +0400

    Add Configuration interface prototype

---
+++ EarthquakeMonitor/src/main/java/edu/atlas/earthquake/config/GlobalConfiguration.java
@@ -0,0 +1,14 @@
+package edu.atlas.earthquake.config;
+
+import edu.atlas.common.config.Configuration;
+
+/**
+ * Created by Nikita Pavlov on 22.01.14
+ */
+public class GlobalConfiguration implements Configuration {
+
+    @Override
+    public void loadFromPropertiesFile(String filename) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
---
+++ TsunamiAtlasCommon/src/main/java/edu/atlas/common/config/Configuration.java
@@ -0,0 +1,8 @@
+package edu.atlas.common.config;
+
+/**
+ * Created by Nikita Pavlov on 22.01.14
+ */
+public interface Configuration {
+    void loadFromPropertiesFile(String filename);
+}
