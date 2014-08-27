97450e87488178d0f8e5c1cd6e2f508611c3fc59
GromHoll <gromholl@gmail.com>
2014-08-27 23:32:45 +0400

    Moved common module to lombok

--- TsunamiAtlasCommon/src/main/java/edu/atlas/common/data/event/DataChangedEvent.java
+++ TsunamiAtlasCommon/src/main/java/edu/atlas/common/data/event/DataChangedEvent.java
@@ -1,28 +1,15 @@
 package edu.atlas.common.data.event;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 import java.util.Collection;
 
+@AllArgsConstructor
 public class DataChangedEvent<T> {
 
-    private Collection<T> allData;
-    private Collection<T> changedData;
-    private Collection<T> newData;
+    @Getter private Collection<T> allData;
+    @Getter private Collection<T> changedData;
+    @Getter private Collection<T> newData;
 
-    public DataChangedEvent(Collection<T> allData, Collection<T> changedData, Collection<T> newData) {
-        this.allData = allData;
-        this.changedData = changedData;
-        this.newData = newData;
-    }
-
-    public Collection<T> getAllData() {
-        return allData;
-    }
-
-    public Collection<T> getChangedData() {
-        return changedData;
-    }
-
-    public Collection<T> getNewData() {
-        return newData;
-    }
 }
